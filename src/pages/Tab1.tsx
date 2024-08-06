c877fd69370660a41172c1a6569057343667c50f
charithAmila <dkcadissanayaka@gmail.com>
2024-08-06 01:23:14 -0400

    changes

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,4 @@
 import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 import {
